f3b5bfb539d6272d5d3ed51f464f1b74e4ee2b3c
Fabian Günaydin <fab.guen@web.de>
2021-05-14 16:45:09 +0200

    Ab4-5b

---
+++ Abgaben/Uebungsblatt04/Einzelabgaben/Günaydin/Team14/Data/Skill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team14.Data
+{
+    public class Skill 
+    {
+        public int skillId { get; set; }
+
+        public String Name { get; set; }
+
+        public String Hardskill { get; set; }
+
+        public String Softskill { get; set; }
+    }
+}
