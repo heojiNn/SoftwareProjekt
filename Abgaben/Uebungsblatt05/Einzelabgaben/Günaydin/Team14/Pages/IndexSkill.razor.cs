0bab03ad2617bd1ee51de90554d9ff858335e157
Fabian Günaydin <fab.guen@web.de>
2021-05-16 15:13:47 +0200

    AB5-Günaydin

---
+++ Abgaben/Uebungsblatt05/Einzelabgaben/Günaydin/Team14/Pages/IndexSkill.razor.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Team14.Data;
+
+namespace Team14.Pages
+{
+    public partial class  IndexSkill
+    {
+        public IndexSkill(ISkillService SkillService)
+        {
+
+        }
+       
+    }
+}
