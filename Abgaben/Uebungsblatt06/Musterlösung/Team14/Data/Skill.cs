a4ea082b1e4feb87150ca63c2806cf790fff2f60
Mario Melle <mario.melle@student.uni-augsburg.de>
2021-05-23 17:22:56 +0200

    Erweiter Skill um Validation in MustL

--- Abgaben/Uebungsblatt06/Musterlösung/Team14/Data/Skill.cs
+++ Abgaben/Uebungsblatt06/Musterlösung/Team14/Data/Skill.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 namespace Team14.Data
 {
     public class Skill
     {
         [Key]
         public int Id { get; set; }
+        [SkillNameConventionAttribut]
         public string Name { get; set; }
         public SkillCatgeory Skilltype { get; set; }
 
@@ -24,7 +26,10 @@ namespace Team14.Data
         public BetriebssystemeCloudPlattformenHardware BetriebssystemeCloudPlattformenHardware { get; set; }
         public string[] SchnittstellenundProtokolle { get; set; }
         public string[] Expertise { get; set; }
+        
     }
+    
+    
     public class SprachenUndFrameworks
     {
         public string[] Sprachen { get; set; }
@@ -70,4 +75,26 @@ namespace Team14.Data
         public string[] Hardware { get; set; }
         public string[] Plattformen { get; set; }
     }
+    
+    
+    
+    
+    
+            public class SkillNameConventionAttribut : ValidationAttribute
+        {
+
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                var skill = (Skill)validationContext.ObjectInstance;
+                var name = value as string;
+
+                string pattern = @"^[A-Za-zÄäÖöÜü ]+$";
+                if (skill.Skilltype == Skill.SkillCatgeory.Softskill && !Regex.IsMatch(name, pattern))
+                {
+                    return new ValidationResult("nur deutsch");
+                }
+
+                return ValidationResult.Success;
+            }
+        }
 }
