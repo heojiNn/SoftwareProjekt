e55f80f311ca1ba7d6619bc6d01bc60c1287276d
Fabian Günaydin <fab.guen@web.de>
2021-05-25 16:49:58 +0200

    added Günaydin/Unit Test

--- Abgaben/Uebungsblatt06/Einzelabgaben/Günaydin/Team14/Data/Services/SkillServiceSimple.cs
+++ Abgaben/Uebungsblatt06/Einzelabgaben/Günaydin/Team14/Data/Services/SkillServiceSimple.cs
@@ -8,22 +8,22 @@ namespace Team14.Data
     //
     public class SkillServiceSimple : ISkillService
     {
-        private readonly Stack<Skill> theSigelton;
+        private readonly Stack<Skill> theSingelton;
         public SkillServiceSimple(Stack<Skill> allSkillsforevaSigelton)
         {
-            theSigelton = allSkillsforevaSigelton;
+            theSingelton = allSkillsforevaSigelton;
         }
 
         //Gibt einen Skill basierend auf der gewuenschten ID zurueck
         //oder null,wenn kein Skill existiert.
         public Skill GetSkill(int Id)
         {
-            return theSigelton.ToList().Find(x => x.Id == Id);
+            return theSingelton.ToList().Find(x => x.Id == Id);
         }
         //Gibt alle verfuegbaren Skills zurueck.
         public IEnumerable<Skill> GetAllSkills()
         {
-            return theSigelton;
+            return theSingelton;
         }
         //Aktualisiert den gegeben Skill bei der Datenquelle
         //oder fueg teinen Neuen ein,wenn die ID noch nicht
@@ -32,7 +32,7 @@ namespace Team14.Data
             var isUpdating = null != GetSkill(skill.Id);
 
             DeleteSkill(skill.Id);
-            theSigelton.Push(skill);
+            theSingelton.Push(skill);
             //Console.WriteLine($"Update {isUpdating} nowhaving size\t{theSigelton.Count}");
             return isUpdating;
         }
@@ -41,12 +41,12 @@ namespace Team14.Data
         {
             if (null != GetSkill(skillId))
             {
-                var copy = theSigelton.ToList();
-                theSigelton.Clear();
+                var copy = theSingelton.ToList();
+                theSingelton.Clear();
                 foreach (Skill s in copy)
                 {
                     if (s.Id != skillId)
-                        theSigelton.Push(s);
+                        theSingelton.Push(s);
                 }
                 return true;
             }
