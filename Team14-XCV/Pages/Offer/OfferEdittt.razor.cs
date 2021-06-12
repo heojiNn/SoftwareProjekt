650f57b38448290573cba4569f5a894de498fd5d
Brigitte Glogger <brigitte.glogger@uni-augsburg.de>
2021-06-12 23:31:09 +0200

    Loginaussehen wie ursprünglich, suche in OfferEdit

---
+++ Team14-XCV/Pages/Offer/OfferEdittt.razor.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Team14.Data;
+
+
+namespace Team14.Pages.Offer
+{
+    public partial class OfferEdittt
+    {
+        private List<Skill> skills;
+        private List<Field> fields;
+
+        private IList<Skill> SelectedHardskills;
+        private IList<Skill> SelectedSoftskills;
+        private IList<Field> SelectedFields;
+
+
+        protected override void OnInitialized()
+        {
+            skills = skillService.GetAllSkills().ToList();
+            fields = fieldService.GetAllFields().ToList();
+
+        }
+
+        private async Task<IEnumerable<Skill>> SearchHardskills(string searchText)
+        {
+
+            return await Task.FromResult(skills.Where(
+                (x => x.Type == SkillGroup.Hardskill && x.Name.ToLower().Contains(searchText.ToLower()))).ToList());
+        }
+
+        private async Task<IEnumerable<Skill>> SearchSoftskills(string searchText)
+        {
+
+            return await Task.FromResult(skills.Where(
+                (x => x.Type == SkillGroup.Softskill && x.Name.ToLower().Contains(searchText.ToLower()))).ToList());
+        }
+        private async Task<IEnumerable<Field>> SearchFields(string searchText)
+        {
+
+            return await Task.FromResult(fields.Where(
+                (x => x.Name.ToLower().Contains(searchText.ToLower()))).ToList());
+        }
+
+
+
+
+
+
+
+        public bool HardskillsCollapsed { get; set; }
+        public bool SoftskillsCollapsed { get; set; }
+        public bool MitarbeiterCollapsed { get; set; }
+        public bool BrancheCollapsed { get; set; }
+
+        void HardskillsToggle()
+        {
+            HardskillsCollapsed = !HardskillsCollapsed;
+        }
+        void SoftskillsToggle()
+        {
+            SoftskillsCollapsed = !SoftskillsCollapsed;
+        }
+        void MitarbeiterToggle()
+        {
+            MitarbeiterCollapsed = !MitarbeiterCollapsed;
+        }
+        void BrancheToggle()
+        {
+            BrancheCollapsed = !BrancheCollapsed;
+        }
+    }
+}
